a9a6b0271189392c8ee95b75b2b869030b482cdd
havryliaksv <gavriliaks@gmail.com>
2023-05-19 15:50:28 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,15 +6,11 @@ const STORAGE_KEY_VIMEO_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('play', onPlay);
+if (localStorage.getItem(STORAGE_KEY_VIMEO_TIME)) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY_VIMEO_TIME));
+}
 
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
-
-function onPlay() {
-  if (localStorage.getItem(STORAGE_KEY_VIMEO_TIME)) {
-    player.setCurrentTime(localStorage.getItem(STORAGE_KEY_VIMEO_TIME));
-  }
-}
 
 function onTimeUpdate() {
   player.getCurrentTime().then(currentTime => {
