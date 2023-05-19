8105925a753819c0f8bf4fd9b5bae0ec78c53c9c
havryliaksv <gavriliaks@gmail.com>
2023-05-19 15:16:31 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY_VIMEO_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', onPlay);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onPlay() {
+  if (localStorage.getItem(STORAGE_KEY_VIMEO_TIME)) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY_VIMEO_TIME));
+  }
+}
+
+function onTimeUpdate() {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem(STORAGE_KEY_VIMEO_TIME, currentTime);
+  });
+}
